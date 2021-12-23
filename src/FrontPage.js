1d063d6e4959861f34b15b6fbe8d56dd84399b3c
sneha-gowda <snehagowda9899@gmail.com>
2021-12-23 16:03:52 +0530

    Assignment-completed

---
+++ src/FrontPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import App from "./App"
+import {Route,Link,Routes} from "react-router-dom"
+const  FrontPage=()=>{
+    return (
+        <>
+            <div className="mainContentpage1">
+                <img src="https://picsum.photos/400/500" alt="" />
+                <div className="textClass">
+                    <h1>10x Team 04</h1>
+                    <Link to="/insta" className="Link"> Enter</Link>
+                </div>
+            </div>
+                <Routes>
+                    
+                    <Route excat path="/insta" element={<App/>} ></Route>
+                    
+                </Routes>
+            
+        </>
+    )
+}
+
+export default FrontPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import FrontPage from './FrontPage'
 import "./index.css"
+import {BrowserRouter,Link,Routes,Route} from "react-router-dom";
 
-ReactDOM.render(<><App/></>, document.getElementById('root'));
+ReactDOM.render(<><BrowserRouter>
+<FrontPage></FrontPage>
+</BrowserRouter></>,
+ document.getElementById('root'));
 
 
